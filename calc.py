0bfc28b6588281910fe17d87e3cd6e023761016e
Mahendra Asapu <mahasapu@in.ibm.com>
2019-09-16 07:05:42 +0000

    Intial Commit

---
+++ calc.py
@@ -0,0 +1,16 @@
+def add(x,y):
+    """Add function"""
+        return x + y
+
+        def sub(x,y):
+            """subtract function"""
+              return x - y
+           
+            def mul(x,y):
+                  """multiply function"""
+                      return x * y
+
+                  def div(x,y):
+                      """divide function"""
+                        return x / y
+
